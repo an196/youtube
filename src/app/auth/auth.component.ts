b8a96c26ea4f0dd4e957fb3eeac47c08d434a01e
an196 <an9x0010@gmail.com>
2023-02-07 17:24:31 +0700

    auto toggle side menu when respone

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,35 @@
-import { AfterViewChecked, Component } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements AfterViewChecked  {
-  showFiller = true;
-  activeSideNav = false;
+export class AuthComponent implements OnInit,AfterViewChecked {
+  showFiller!: boolean;
+  activeSideNav!: boolean;
+  openedSideMenu!: boolean;
 
-  ngAfterViewChecked(): void{
-   
+  @ViewChild('sidemenu') sidemenu!: ElementRef;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const breakpoint = 1312;
+
+    if(event.target.innerWidth < breakpoint && this.openedSideMenu){
+      this.openedSideMenu = false;
+    }
+    if(event.target.innerWidth >= breakpoint && !this.openedSideMenu)
+      this.openedSideMenu = true;
+  }
+
+  ngAfterViewChecked(): void {
+
+  }
+
+  ngOnInit(): void {
+    this.showFiller = true;
+    this.activeSideNav = false;
+    this.openedSideMenu = true;
   }
 }
--- src/app/dashboard-crm/dashboard-crm.component.ts
+++ src/app/dashboard-crm/dashboard-crm.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-crm',
   templateUrl: './dashboard-crm.component.html',
   styleUrls: ['./dashboard-crm.component.scss'],
 })
+
 export class DashboardCrmComponent implements OnInit {
+  numberCard = [1,1,1,1];
+
   constructor() {}
   ngOnInit(): void {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const breakpoint = 1312;
+
+  }
 }
