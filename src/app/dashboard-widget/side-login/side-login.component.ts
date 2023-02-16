d5cbf0f1165066feac28592a2a6df43030578856
an196 <an9x0010@gmail.com>
2023-02-16 11:45:35 +0700

    fix responsive on mobile

--- src/app/dashboard-widget/side-login/side-login.component.ts
+++ src/app/dashboard-widget/side-login/side-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
+import { AuthService } from 'app/shared/services/auth-service';
 
 const THREEDOTS_ICON = `<svg viewBox="0 0 24 24"><g><path d="M12,16.5c0.83,0,1.5,0.67,1.5,1.5s-0.67,1.5-1.5,1.5s-1.5-0.67-1.5-1.5S11.17,16.5,12,16.5z M10.5,12 c0,0.83,0.67,1.5,1.5,1.5s1.5-0.67,1.5-1.5s-0.67-1.5-1.5-1.5S10.5,11.17,10.5,12z M10.5,6c0,0.83,0.67,1.5,1.5,1.5 s1.5-0.67,1.5-1.5S12.83,4.5,12,4.5S10.5,5.17,10.5,6z"></path></g></svg>  `;
 const USER_ICON =
@@ -14,7 +15,7 @@ const USER_ICON =
 export class SideLoginComponent implements OnInit {
 
 
-    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, public authService: AuthService) {
         iconRegistry.addSvgIconLiteral(
             'threedots',
             sanitizer.bypassSecurityTrustHtml(THREEDOTS_ICON)
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,14 @@
 export const environment = {
     production: false,
     baseUrl: 'https://youtube-api-chd6.onrender.com',
-    localUrl: 'http://localhost:8080'
+    localUrl: 'http://localhost:8080',
+    firebase: {
+        apiKey: "AIzaSyD1Qg3JlntjSNf5QRW9aeBO-C6V_uRjIhw",
+        authDomain: "clone-377414.firebaseapp.com",
+        projectId: "youtube-clone-377414",
+        storageBucket: "youtube-clone-377414.appspot.com",
+        messagingSenderId: "701137348902",
+        appId: "1:701137348902:web:a8ad6d2ddf97992289b9e7",
+        measurementId: "G-ERNE6DPZ2C"
+      }
 };
